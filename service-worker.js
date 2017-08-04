94163bcc1910c39cd2caf7785463faf56c2b75ea
Anuraj <anuraj.p@msn.com>
2017-08-04 13:25:27 +0530

    Service worker added for caching purposes.

---
+++ service-worker.js
@@ -0,0 +1,59 @@
+const PRECACHE = 'precache-v1';
+const RUNTIME = 'runtime';
+
+const PRECACHE_URLS = [
+	'index.html',
+	'./',
+	'css/bootstrap.min.css',
+	'js/jquery.min.js',
+	'images/mvplogo.png',
+	'favicon.ico',
+	'js/pace.min.js',
+	'js/bootstrap.min.js',
+	'js/ie10-viewport-bug-workaround.js',
+	'images/social.png'
+];
+
+
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(PRECACHE)
+      .then(cache => cache.addAll(PRECACHE_URLS))
+      .then(self.skipWaiting())
+  );
+});
+
+
+self.addEventListener('activate', event => {
+  const currentCaches = [PRECACHE, RUNTIME];
+  event.waitUntil(
+    caches.keys().then(cacheNames => {
+      return cacheNames.filter(cacheName => !currentCaches.includes(cacheName));
+    }).then(cachesToDelete => {
+      return Promise.all(cachesToDelete.map(cacheToDelete => {
+        return caches.delete(cacheToDelete);
+      }));
+    }).then(() => self.clients.claim())
+  );
+});
+
+self.addEventListener('fetch', event => {
+  if (event.request.url.startsWith(self.location.origin)) {
+    event.respondWith(
+      caches.match(event.request).then(cachedResponse => {
+        if (cachedResponse) {
+          return cachedResponse;
+        }
+
+        return caches.open(RUNTIME).then(cache => {
+          return fetch(event.request).then(response => {
+            // Put a copy of the response in the runtime cache.
+            return cache.put(event.request, response.clone()).then(() => {
+              return response;
+            });
+          });
+        });
+      })
+    );
+  }
+});
