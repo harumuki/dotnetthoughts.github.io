da76d1d0579681b3d069853df439621e536ffbf6
Anuraj <anuraj.p@msn.com>
2017-09-24 17:24:17 +0530

    Version string changed

--- service-worker.js
+++ service-worker.js
@@ -5,7 +5,7 @@ layout: none
 
 const SW_VERSION = '{{ site.time | date: "%Y.%m.%d"}}';
 const PRECACHE = 'precache-v1';
-const RUNTIME = 'v{{ site.time | date: "%Y%m%d"}}';
+const RUNTIME = '{{ site.time | date: "%Y-%m-%d"}}';
 
 const PRECACHE_URLS = [
     '/',
