1b3e8ab20a22f6b7a7c841ea676fd6d7085c0bf1
Anuraj <anuraj.p@msn.com>
2017-12-12 16:19:10 +0530

    Service worker modified

--- service-worker.js
+++ service-worker.js
@@ -2,69 +2,115 @@
 layout: none
 ---
 
+'use strict';
 
-const SW_VERSION = '{{ site.time | date: "%Y.%m.%d"}}';
-const PRECACHE = 'precache-v10';
-const RUNTIME = '{{ site.time | date: "%Y-%m-%d"}}';
+// Licensed under a CC0 1.0 Universal (CC0 1.0) Public Domain Dedication
+// http://creativecommons.org/publicdomain/zero/1.0/
 
-const PRECACHE_URLS = [
-    '/css/bootstrap.min.css',
-    '/css/site.css',
-    'js/jquery.min.js',
-    'images/mvplogo.png',
-    'favicon.ico',
-    'js/pace.min.js',
-    'js/bootstrap.min.js',
-    'js/ie10-viewport-bug-workaround.js',
-    'images/social.png',
-    'fonts/open-sans-v14-latin-regular.eot',
-    'fonts/open-sans-v14-latin-regular.svg',
-    'fonts/open-sans-v14-latin-regular.ttf',
-    'fonts/open-sans-v14-latin-regular.woff',
-    'fonts/open-sans-v14-latin-regular.woff2',
-    'images/offline.png',
-    'offline.html'
-];
+(function() {
 
-self.addEventListener('install', event => {
-  event.waitUntil(
-    caches.open(PRECACHE)
-      .then(cache => cache.addAll(PRECACHE_URLS))
-      .then(self.skipWaiting())
-  );
-});
+    // Update 'version' if you need to refresh the cache
+    var staticCacheName = 'static';
+    var version = '{{ site.time | date: "%Y.%m.%d"}}';
+    function updateStaticCache() {
+        return caches.open(version + staticCacheName)
+            .then(function (cache) {
+                return cache.addAll([
+                  '/css/bootstrap.min.css',
+                  '/css/site.css',
+                  'js/jquery.min.js',
+                  'images/mvplogo.png',
+                  'favicon.ico',
+                  'js/pace.min.js',
+                  'js/bootstrap.min.js',
+                  'js/ie10-viewport-bug-workaround.js',
+                  'images/social.png',
+                  'fonts/open-sans-v14-latin-regular.eot',
+                  'fonts/open-sans-v14-latin-regular.svg',
+                  'fonts/open-sans-v14-latin-regular.ttf',
+                  'fonts/open-sans-v14-latin-regular.woff',
+                  'fonts/open-sans-v14-latin-regular.woff2',
+                  'images/offline.png',
+                  'offline.html',
+                  '/',
+                  'index.html'
+                ]);
+            });
+    };
 
-self.addEventListener('activate', event => {
-  const currentCaches = [PRECACHE, RUNTIME];
-  event.waitUntil(
-    caches.keys().then(cacheNames => {
-      return cacheNames.filter(cacheName => !currentCaches.includes(cacheName));
-    }).then(cachesToDelete => {
-      return Promise.all(cachesToDelete.map(cacheToDelete => {
-        return caches.delete(cacheToDelete);
-      }));
-    }).then(() => self.clients.claim())
-  );
-});
+    self.addEventListener('install', function (event) {
+        event.waitUntil(updateStaticCache());
+    });
 
-self.addEventListener('fetch', event => {
-  if (event.request.url.startsWith(self.location.origin)) {
-    event.respondWith(
-      caches.match(event.request).then(cachedResponse => {
-        if (cachedResponse) {
-          return cachedResponse;
+    self.addEventListener('activate', function (event) {
+        event.waitUntil(
+            caches.keys()
+                .then(function (keys) {
+                    return Promise.all(keys
+                        .filter(function (key) {
+                          return key.indexOf(version) !== 0;
+                        })
+                        .map(function (key) {
+                          return caches.delete(key);
+                        })
+                    );
+                })
+        );
+    });
+
+    self.addEventListener('fetch', function (event) {
+        var request = event.request;
+        if (request.method !== 'GET') {
+            event.respondWith(
+                fetch(request)
+                    .catch(function () {
+                        return caches.match('/offline.html');
+                    })
+            );
+            return;
         }
 
-        return caches.open(RUNTIME).then(cache => {
-          return fetch(event.request).then(response => {
-            return cache.put(event.request, response.clone()).then(() => {
-              return response;
-            });
-          }).catch(function(){
-            return caches.match('/offline.html');
-          });
-        });
-      })
-    );
-  }
-});
+        if (request.headers.get('Accept').indexOf('text/html') !== -1) {
+            if (request.mode != 'navigate') {
+                request = new Request(request.url, {
+                    method: 'GET',
+                    headers: request.headers,
+                    mode: request.mode,
+                    credentials: request.credentials,
+                    redirect: request.redirect
+                });
+            }
+            event.respondWith(
+                fetch(request)
+                    .then(function (response) {
+                        var copy = response.clone();
+                        caches.open(version + staticCacheName)
+                            .then(function (cache) {
+                                cache.put(request, copy);
+                            });
+                        return response;
+                    })
+                    .catch(function () {
+                        return caches.match(request)
+                            .then(function (response) {
+                                return response || caches.match('/offline.html');
+                            })
+                    })
+            );
+            return;
+        }
+
+        event.respondWith(
+            caches.match(request)
+                .then(function (response) {
+                    return response || fetch(request)
+                        .catch(function () {
+                            if (request.headers.get('Accept').indexOf('image') !== -1) {
+                                return new Response('<svg width="400" height="300" role="img" aria-labelledby="offline-title" viewBox="0 0 400 300" xmlns="http://www.w3.org/2000/svg"><title id="offline-title">Offline</title><g fill="none" fill-rule="evenodd"><path fill="#D8D8D8" d="M0 0h400v300H0z"/><text fill="#9B9B9B" font-family="Helvetica Neue,Arial,Helvetica,sans-serif" font-size="72" font-weight="bold"><tspan x="93" y="172">offline</tspan></text></g></svg>', { headers: { 'Content-Type': 'image/svg+xml' }});
+                            }
+                        });
+                })
+        );
+    });
+
+})();
