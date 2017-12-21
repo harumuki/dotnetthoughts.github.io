6d3b5bb7d1047980d86a51c56fa8e3eba1819bb9
Anuraj <anuraj.p@msn.com>
2017-12-21 05:50:20 +0530

    Service worker version update

--- serviceworker.js
+++ serviceworker.js
@@ -1,6 +1,5 @@
-var CACHE_NAME = 'blog-cache-v1.1';
+var CACHE_NAME = 'blog-cache-v2';
 var urlsToCache = [
-'/',
 'index.html',
 '/css/bootstrap.min.css',
 '/css/site.css',
@@ -24,7 +23,6 @@ self.addEventListener('install', function(event) {
     event.waitUntil(
         caches.open(CACHE_NAME)
         .then(function(cache) {
-            console.log('Opened cache');
             return cache.addAll(urlsToCache);
         })
     );
